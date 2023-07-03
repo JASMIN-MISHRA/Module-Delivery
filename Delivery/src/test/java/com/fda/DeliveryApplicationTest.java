83424f5b7df6a000d4d13b9fe4594ee721586729
Jasmin Mishra <124661283+JASMIN-MISHRA@users.noreply.github.com>
2023-07-03 13:52:06 +0530

    Update DeliveryApplicationTest.java

--- Delivery/src/test/java/com/fda/DeliveryApplicationTest.java
+++ Delivery/src/test/java/com/fda/DeliveryApplicationTest.java
@@ -12,54 +12,5 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest
 public class DeliveryApplicationTest {
 
-    @Test
-    public void testDeliveryCreation() {
-        // Create a mock delivery agent
-        DeliveryAgent deliveryAgentMock = mock(DeliveryAgent.class);
-
-        // Create a delivery instance
-        Delivery delivery = new Delivery(0, null, deliveryAgentMock);
-
-        // Verify that the delivery agent was set correctly
-        assertEquals(deliveryAgentMock, delivery.getDeliveryAgent());
-    }
-
-    @Test
-    public void testStartDelivery() {
-        // Create a mock delivery agent
-        DeliveryAgent deliveryAgentMock = mock(DeliveryAgent.class);
-
-        // Create a delivery instance
-        Delivery delivery = new Delivery(0, null, deliveryAgentMock);
-
-        // Start the delivery
-        delivery.start();
-
-        // Verify that the delivery agent's startDelivery method was called
-        verify(deliveryAgentMock).startDelivery();
-    }
-
-    @Test
-    public void testCompleteDelivery() {
-        // Create a mock delivery agent
-        DeliveryAgent deliveryAgentMock = mock(DeliveryAgent.class);
-
-        // Create a delivery instance
-        Delivery delivery = new Delivery(0, null, deliveryAgentMock);
-
-        // Complete the delivery
-        delivery.complete();
-
-        // Verify that the delivery agent's completeDelivery method was called
-        verify(deliveryAgentMock).completeDelivery();
-    }
-
-	
-
-		
-		
-
-		
-
-
-	}
+    
+}
